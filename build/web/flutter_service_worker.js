9e26e10e976a08476ec1a060f7a19331efa445bf
SultanTnn <rvxbeon@gmail.com>
2025-10-22 10:18:38 +0700

    update hasil build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -51,7 +51,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d28d604a450015d7d38379aa44af2839
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "45f10aea27b0a35111178b7830785d15",
+"flutter_bootstrap.js": "cea980461843ade34b746bd755acb084",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
